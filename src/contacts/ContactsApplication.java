f2783872591893a9805404f7b10a1a9accbb45bb
Denzel-Britton <denzel.britton573@gmail.com>
2022-08-18 15:28:35 -0500

    propmt finished

--- src/contacts/ContactsApplication.java
+++ src/contacts/ContactsApplication.java
@@ -5,7 +5,7 @@ public class ContactsApplication {
 
         boolean keepSearching = true;
         do {
-            System.out.println("Please Enter an option (1, 2, 3, 4 or 5):");
+
             Input input = new Input();
             System.out.println("1 - View contacts.");
             System.out.println("2 - Add a new contact");
@@ -20,21 +20,16 @@ public class ContactsApplication {
                 case 2 -> addContacts();
                 case 3 -> searchContacts();
                 case 4 -> deleteContact();
-                case 5 -> exit();
+              case 5 -> {
+                  System.out.println("Goodbye");
+                    return;
+                }
             }
-            System.out.println("Are you sure you want to leave [y/n]");
-            String letsContinue = input.getString();
-            if (letsContinue.contains("y") || letsContinue.contains("Y")) {
-                System.out.println("Goodbye!");
-                keepSearching = false;
-                break;
-            }
+
         } while (keepSearching);
     }
 
-    private static void exit() {
-        System.out.println("5 works exits ");
-    }
+
 
     private static void deleteContact() {
         System.out.println("4 works");
