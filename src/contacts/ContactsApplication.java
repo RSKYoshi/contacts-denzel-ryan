9097287bd5949311591b27796be77351e9c8b230
Denzel-Britton <denzel.britton573@gmail.com>
2022-08-18 11:48:23 -0500

    my branch test

--- src/contacts/ContactsApplication.java
+++ src/contacts/ContactsApplication.java
@@ -2,7 +2,7 @@ package contacts;
 
 public class ContactsApplication {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test one a new branch");
 
     }
 }
