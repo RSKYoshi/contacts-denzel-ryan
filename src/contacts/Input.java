800cdbf9a9514d3ed0766b3eca26dd5c7c290445
Denzel-Britton <denzel.britton573@gmail.com>
2022-08-22 11:15:25 -0500

    refactored and cleaned code

--- src/contacts/Input.java
+++ src/contacts/Input.java
@@ -24,14 +24,14 @@ public class Input {
     }
 
 
-    public int getInt(String prompt) {
+    public Long getInt(String prompt) {
 //        while (true) {
         System.out.print(prompt);
         try {
-            int userInput = Integer.valueOf(getString(""));
+            Long userInput = Long.valueOf(getString(""));
             return userInput;
         } catch (NumberFormatException e) {
-            System.out.println("Invalid input");
+            System.out.println("Invalid input please try again");
             System.out.println(e.getMessage());
         }
 //        }
